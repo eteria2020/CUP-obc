972f8ba9391dd1a5f82f9811bdae6c564cc3ee20
Fulvio <fulvio.bombara@sharengo.eu>
2018-02-06 14:48:33 +0100

    wrong icon in fmenu keyCheck, changed max time in pin check

--- oBC/src/main/java/eu/philcar/csg/OBC/controller/map/FMenu.java
+++ oBC/src/main/java/eu/philcar/csg/OBC/controller/map/FMenu.java
@@ -351,12 +351,12 @@ public class FMenu extends FBase implements OnClickListener {
 				case PARK_OFF: 		// (FALSE, NULL, OFF)
 					if (activity.checkisInsideParkingArea()) {
 						if( CarInfo.keyStatus != null && !CarInfo.keyStatus.equalsIgnoreCase("OFF") && App.checkKeyOff) {
-							UIHelper(R.drawable.sel_button_cancel, R.string.menu_rent_end_key_on, null,
+							UIHelper(R.drawable.sel_button_cancel_small, R.string.menu_rent_end_key_on, null,
 									R.drawable.button_rent_pause_pushed, R.string.menu_park_mode_suspend_key_on, null,
 									R.drawable.sel_button_fuel_station_small, R.string.menu_refuel, this,
 									R.drawable.sel_button_back, this);
 						}else {
-							UIHelper(R.drawable.sel_button_cancel, R.string.menu_rent_end, this,
+							UIHelper(R.drawable.sel_button_cancel_small, R.string.menu_rent_end, this,
 									R.drawable.sel_button_rent_pause, R.string.menu_park_mode_suspend, this,
 									R.drawable.sel_button_fuel_station_small, R.string.menu_refuel, this,
 									R.drawable.sel_button_back, this);
