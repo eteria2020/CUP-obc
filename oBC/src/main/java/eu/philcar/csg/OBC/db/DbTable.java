fd20cba178be6ead2bd1e9bf4df5a701b566c176
Fulvio <fulvio.bombara@sharengo.eu>
2018-02-16 12:52:43 +0100

    little upgrade on updateMany

--- oBC/src/main/java/eu/philcar/csg/OBC/db/DbTable.java
+++ oBC/src/main/java/eu/philcar/csg/OBC/db/DbTable.java
@@ -4,6 +4,7 @@ import android.os.Parcelable;
 
 import java.sql.SQLException;
 import java.util.Collection;
+import java.util.concurrent.Callable;
 
 import com.j256.ormlite.dao.BaseDaoImpl;
 import com.j256.ormlite.support.ConnectionSource;
@@ -26,21 +27,28 @@ public class DbTable<tableClass, pk>  extends BaseDaoImpl<tableClass, pk>{
 
 	public Observable<tableClass> createOrUpdateMany(final Collection<tableClass> collection){
 		return Observable.create(emitter -> {
-			if (emitter.isDisposed()) return;
-
-			try {
-				for (tableClass row : collection) {
-					if(row instanceof CustomOp){
-						((CustomOp) row).onDbWrite();
+			if (emitter.isDisposed())
+				return;
+			callBatchTasks((Callable<Void>) () -> {
+				try {
+					for (tableClass row : collection) {
+						if(row instanceof CustomOp){
+							((CustomOp) row).onDbWrite();
+						}
+						int result = createOrUpdate(row).getNumLinesChanged();
+						if (result >= 0) emitter.onNext(row);
 					}
-					int result = createOrUpdate(row).getNumLinesChanged();
-					if (result >= 0) emitter.onNext(row);
+					emitter.onComplete();
+				} catch(Exception e) {
+					DLog.E("Exception updating Customer",e);
+					emitter.onError(e);
 				}
-				emitter.onComplete();
-			} catch(Exception e) {
-				DLog.E("Exception updating Customer",e);
-				emitter.onError(e);
-			}
+
+				return null;
+
+
+			});
+
 		});
 	}
 	
